2010513e7e0d5fc0b913aad7dd68dc970832f2a5
OlgaVyshnevetska <72294661+OlgaVyshnevetska@users.noreply.github.com>
2021-07-10 02:09:11 +0300

    Add files via upload

---
+++ WorkDayCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime temp = startDate;
+            for(int i = 0; i < dayCount; i++)
+            {
+                if(weekEnds == null)
+                {
+                    temp = temp.AddDays(1);
+                }
+                else
+                {
+                    for(int j = 0; j < weekEnds.Length; j++)
+                    {
+                        if(temp == weekEnds[j].StartDate)
+                        {
+                            temp = weekEnds[j].EndDate;
+                            temp = temp.AddDays(1);
+                        }
+                        else
+                        {
+                            temp = temp.AddDays(1);
+                        }
+                    }
+                }
+            }
+            DateTime result = temp;
+            return result;
+            //throw new NotImplementedException();
+        }
+    }
+}
